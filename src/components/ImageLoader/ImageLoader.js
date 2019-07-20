8e4b8484ba995e7362ae2d4eac52ff9fc64b29a2
Artem Shcherbina <scherbina.ar@gmail.com>
2019-07-20 16:21:02 +0200

    add loading spinner

--- src/components/ImageLoader/ImageLoader.js
+++ src/components/ImageLoader/ImageLoader.js
@@ -1,4 +1,5 @@
-import React, {useCallback, useState} from 'react';
+import React, {useCallback, useState} from 'react'
+import Loader from 'react-loader-spinner'
 import {PUBLIC_URL} from '../../constants/settings'
 import defaultImage from '../../images/default_image.jpg'
 import './ImageLoader.css'
@@ -6,15 +7,19 @@ import './ImageLoader.css'
 function ImageLoader() {
   const [image, setImage] = useState(defaultImage);
   const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
   const getImage = useCallback(async () => {
     setError(false);
+    setLoading(true);
     try {
       const response = await fetch(PUBLIC_URL);
-      const data = await response.json()
-      setImage(data.message)
+      const data = await response.json();
+      setImage(data.message);
+      setLoading(false);
     } catch(err) {
       console.log(err)
       setError(true);
+      setLoading(false);
     }
   });
 
@@ -25,6 +30,16 @@ function ImageLoader() {
         <img src={image} alt={"A dog"} />
         { error && <div style={{color: `red`}}>some error occurred, while fetching image</div> }
       </div>
+      { loading &&
+        <div className="image-container__loading">
+          <Loader 
+            type="Rings"
+            color="#00BFFF"
+            height="100"	
+            width="100"
+          />
+        </div>
+      } 
     </div>
   )
 }
